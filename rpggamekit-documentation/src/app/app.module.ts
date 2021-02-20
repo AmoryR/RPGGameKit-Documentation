c1e63019934e5ff1d63c8c32812acf1dfd977004
Amory <theonlygeeking@gmail.com>
2021-02-20 10:37:13 +0100

    Using material and drawer app container setup

--- rpggamekit-documentation/src/app/app.module.ts
+++ rpggamekit-documentation/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+
+		MatSidenavModule,
+		MatButtonModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
